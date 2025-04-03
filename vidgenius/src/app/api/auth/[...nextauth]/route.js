2abd690f24042a544d2c82a4de9d5a8f3d0561fb
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-04-04 00:52:00 +0530

    created new folder called api for creating APIs for the System

---
+++ vidgenius/src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+const handler=NextAuth ({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  pages:{
+    signIn:"/signin"
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
+export { handler as GET, handler as POST };
